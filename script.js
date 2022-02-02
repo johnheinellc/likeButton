3bf388bdbf7c20acdc118b7e5372173a7abf1dd4
johnheinellc <johnheinellc@gmail.com>
2022-02-01 22:16:27 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// button 1
+var count = 1;
+var countElement = document.querySelector("#count");
+console.log(countElement);
+
+function add1() {
+    count++;
+    countElement.innerText = count + " Like(s)";
+    console.log(count);
+}
+// button 2
+var count2 = 1;
+var count2Element = document.querySelector("#count2");
+console.log(count2Element);
+
+function add2() {
+    count2++;
+    count2Element.innerText = count2 + " Like(s)";
+    console.log(count2);
+}
+// button 3
+var count3 = 1;
+var count3Element = document.querySelector("#count3");
+console.log(count3Element);
+
+function add3() {
+    count3++;
+    count3Element.innerText = count3 + " Like(s)";
+    console.log(count3);
+}
